de556336c61e76856707f848d499b2e0978a66c0
mitchell28 <116350035+mitchell28@users.noreply.github.com>
2025-05-12 13:16:03 +0100

    Add missing Services and CaseStudies components

---
+++ src/components/ui/CaseStudies.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const CaseStudies = () => {
+  return <div className="text-xl">Case Studies Section</div>
+}
---
+++ src/components/ui/Services.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Services = () => {
+  return <div className="text-xl">Services Section</div>
+}
